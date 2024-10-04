a69f5d1b56b0e56e7fb3547f7027d075c6214da3
ichichou <35998815+ichichou@users.noreply.github.com>
2024-10-04 21:01:43 +0900

    Add MTGAP layer on Vega

--- keyboards/ai03/vega/keymaps/ichichou/keymap.c
+++ keyboards/ai03/vega/keymaps/ichichou/keymap.c
@@ -23,23 +23,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define HYPR_BSPC RHYPR_T(KC_BSPC)
 #define CTL_ESC   CTL_T(KC_ESC)
 #define CTL_ENT   CTL_T(KC_ENT)
-#define CTL_LBRC  CTL_T(KC_LBRC)
 #define SFT_SPC   SFT_T(KC_SPC)
-#define SFT_RBRC  SFT_T(KC_RBRC)
 #define SFT_CW    SFT_T(CW_TOGG)
 #define GUI_LNG2  GUI_T(KC_LNG2)
 #define RGUI_LNG1 RGUI_T(KC_LNG1)
 
 enum layer_names {
   _BASE,
+  _MTGAP,
   _FN,
 };
 
-// // Custom Keycodes
-// enum my_keycodes {
-//   TEST_1 = SAFE_RANGE,
-//   TEST_2,
-// };
+// Custom Keycodes
+enum my_keycodes {
+  TEST_1 = SAFE_RANGE,
+};
 
 // Behavior of Any Keycode
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -99,19 +97,27 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_all(
-    KC_ESC,            KC_1,     KC_2, KC_3,   KC_4,   KC_5,    KC_6,    KC_7,   KC_8, KC_9,    KC_0,      KC_MINS, KC_EQL,  HYPR_BSPC, KC_DEL,  KC_DEL,
-    HYPR_TAB,          KC_Q,     KC_W, KC_E,   KC_R,   KC_T,    KC_GRV,  KC_Y,   KC_U, KC_I,    KC_O,      KC_P,    KC_QUOT, KC_BSLS,            KC_GRV,
-    CTL_ESC,           KC_A,     KC_S, KC_D,   KC_F,   KC_G,    KC_RBRC, KC_H,   KC_J, KC_K,    KC_L,      KC_SCLN,          CTL_ENT,            KC_TILD,
-    SFT_CW,   KC_BSLS, KC_Z,     KC_X, KC_C,   KC_V,   KC_B,    KC_LBRC, KC_N,   KC_M, KC_COMM, KC_DOT,    KC_SLSH,          KC_UP,              MO(_FN),
-    MO(_FN),  KC_LALT, GUI_LNG2,       KC_SPC,         SFT_SPC,          KC_SPC,       KC_RALT, RGUI_LNG1,          KC_LEFT, KC_DOWN,            KC_RGHT
+    KC_ESC,            KC_1,     KC_2, KC_3,   KC_4,   KC_5,    KC_6,    KC_7,   KC_8, KC_9,    KC_0,      KC_MINS, KC_EQL,  HYPR_BSPC, KC_DEL, KC_DEL,
+    HYPR_TAB,          KC_Q,     KC_W, KC_E,   KC_R,   KC_T,    KC_GRV,  KC_Y,   KC_U, KC_I,    KC_O,      KC_P,    KC_QUOT, KC_BSLS,           KC_GRV,
+    CTL_ESC,           KC_A,     KC_S, KC_D,   KC_F,   KC_G,    KC_RBRC, KC_H,   KC_J, KC_K,    KC_L,      KC_SCLN,          CTL_ENT,           KC_TILD,
+    SFT_CW,   KC_BSLS, KC_Z,     KC_X, KC_C,   KC_V,   KC_B,    KC_LBRC, KC_N,   KC_M, KC_COMM, KC_DOT,    KC_SLSH,          KC_UP,             MO(_FN),
+    MO(_FN),  KC_LALT, GUI_LNG2,       KC_SPC,         SFT_SPC,          KC_SPC,       KC_RALT, RGUI_LNG1,          KC_LEFT, KC_DOWN,           KC_RGHT
+  ),
+
+  [_MTGAP] = LAYOUT_all(
+    KC_ESC,            KC_1,      KC_2,    KC_3,    KC_4,   KC_5,     KC_6,    KC_7,   KC_8, KC_9,    KC_0,      KC_MINS, KC_EQL,  HYPR_BSPC, KC_DEL, KC_DEL,
+    HYPR_TAB,          KC_Y,      KC_P,    KC_O,    KC_U,   KC_SCLN,  KC_GRV,  KC_K,   KC_D, KC_L,    KC_C,      KC_W,    KC_Q,    KC_BSLS,           KC_GRV,
+    CTL_ESC,           KC_I,      KC_N,    KC_E,    KC_A,   KC_COMM,  KC_RBRC, KC_M,   KC_H, KC_T,    KC_S,      KC_R,             CTL_ENT,           KC_TILD,
+    SFT_CW,   KC_BSLS, KC_Z,      KC_SLSH, KC_QUOT, KC_DOT, KC_X,     KC_LBRC, KC_B,   KC_F, KC_G,    KC_V,      KC_J,             KC_UP,             MO(_FN),
+    MO(_FN),  KC_LALT, GUI_LNG2,           KC_SPC,          SFT_SPC,           KC_SPC,       KC_RALT, RGUI_LNG1,          KC_LEFT, KC_DOWN,           KC_RGHT
   ),
 
   [_FN] = LAYOUT_all(
-    LCG(KC_Q),          KC_F1,        KC_F2,     KC_F3,     KC_F4,        KC_F5,   KC_F6,   KC_F7,   KC_F8,        KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DEL,  XXXXXXX, XXXXXXX,
-    _______,            LCA(KC_U),    LCA(KC_I), XXXXXXX,   MEH(KC_C),    XXXXXXX, XXXXXXX, XXXXXXX, LCA(KC_BSPC), XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD,  KC_GRV,           G(KC_UP),
-    _______,            LCA(KC_LEFT), XXXXXXX,   XXXXXXX,   LCA(KC_RGHT), XXXXXXX, XXXXXXX, XXXXXXX, LCA(KC_C),    XXXXXXX, KC_MPRV, KC_MNXT,           KC_MPLY,          G(KC_DOWN),
-    _______,   XXXXXXX, LCA(KC_Z),    LCA(KC_X), LCA(KC_V), LCA(KC_B),    XXXXXXX, QK_BOOT, XXXXXXX, LCA(KC_ENT),  KC_VOLD, KC_VOLU, KC_MUTE,           XXXXXXX,          _______,
-    _______,   _______, _______,                 _______,                 _______,          _______,               _______, _______,          A(KC_UP), XXXXXXX,          A(KC_DOWN)
+    LCG(KC_Q),          KC_F1,     KC_F2,      KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_DEL,  XXXXXXX, XXXXXXX,
+    _______,            DF(_BASE), DF(_MTGAP), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD,  KC_GRV,           G(KC_UP),
+    _______,            XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT,           KC_MPLY,          G(KC_DOWN),
+    _______,   XXXXXXX, XXXXXXX,   XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE,           XXXXXXX,          _______,
+    _______,   _______, _______,               _______,          _______,          _______,          _______, _______,          A(KC_UP), XXXXXXX,          A(KC_DOWN)
   ),
 
 };
