e54ff307af6d3b1d4093643c708a6fef3d4e7054
ichichou <35998815+ichichou@users.noreply.github.com>
2024-11-18 14:08:10 +0900

    Update Cornelius keymap

---
+++ keyboards/kbd_firmware/cornelius/keymaps/ichichou/keymap.bk.c
@@ -0,0 +1,200 @@
+// QMK Code Backup
+
+// -- Define Macros {{{
+//
+// 一部の機能を利用するには rules.mk にコードの記述が必要
+// 例：
+//
+// CAPS_WORD_ENABLE = yes
+// SEND_STRING_ENABLE = yes
+
+// Modifier Keycodes
+#define RHYPR_T(kc) MT(MOD_RCTL | MOD_RSFT | MOD_RALT | MOD_RGUI, kc)
+#define LCG(kc)     (QK_LCTL | QK_LGUI | (kc))
+
+// Tap-Hold Keys
+#define NAV_ESC   LT(_NAV, KC_ESC)
+#define SYM_TAB   LT(_SYM, KC_TAB)
+#define HYPR_TAB  RHYPR_T(KC_TAB)
+#define HYPR_BSPC RHYPR_T(KC_BSPC)
+#define CTL_ESC   CTL_T(KC_ESC)
+#define CTL_ENT   CTL_T(KC_ENT)
+#define CTL_QUOT  CTL_T(KC_QUOT)
+#define CTL_Q     CTL_T(KC_Q)
+#define SFT_SPC   SFT_T(KC_SPC)
+#define SFT_CW    SFT_T(CW_TOGG)
+#define SFT_BSLS  SFT_T(KC_BSLS)
+#define GUI_LNG2  GUI_T(KC_LNG2)
+#define RGUI_LNG1 RGUI_T(KC_LNG1)
+
+// Home Row Mods
+#define CTL_LBRC  CTL_T(KC_LBRC)
+#define SFT_RBRC  SFT_T(KC_RBRC)
+#define ALT_LSG_A ALT_T(LSG(KC_A))
+
+// }}}
+
+// -- Layers {{{
+//
+// 定義したレイヤーを利用しなくても、コメントアウトする必要はない
+
+enum layer_names {
+  _BASE,
+  _MTGAP,
+  _NAV,
+  _SYM,
+  _TRI,
+  _FN,
+};
+
+// Tri Layers
+//
+// 有効化には rules.mk に以下のコードを記述する：
+// TRI_LAYER_ENABLE = yes
+//
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _NAV, _SYM, _TRI);
+}
+
+// }}}
+
+// -- Custom Keycodes {{{
+//
+// 定義したキーコードを利用しなくても、コメントアウトする必要はない
+
+enum my_keycodes {
+  SAMPLE_1 = SAFE_RANGE,
+  SAMPLE_2,
+};
+
+// }}}
+
+// -- Behavior of Any Keycode {{{
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+
+    // Caps Word / Shift
+    case SFT_CW:
+      if (record->tap.count && record->event.pressed) {
+        caps_word_toggle();
+        return false;
+      }
+      return true;
+
+    // Shift-Command-A / Option
+    case ALT_T(LSG(KC_A)):
+      if (record->tap.count && record->event.pressed) {
+        tap_code16(LSG(KC_A));
+        return false;
+      }
+      return true;
+
+    // Otherwise
+    default:
+      return true;
+
+  }
+  return true;
+}
+
+// -- }}}
+
+// -- Combos {{{
+//
+// 有効化には rules.mk に以下のコードを記述する：
+// COMBO_ENABLE = yes
+//
+// 有効化しない場合は keymap.c のコードをコメントアウトする必要あり
+
+const uint16_t PROGMEM ab_combo[] = {KC_A, KC_B, COMBO_END};
+combo_t key_combos[] = {
+  COMBO(ab_combo, KC_ESC),
+};
+
+// }}}
+
+// -- Key Overrides {{{
+//
+// 有効化には rules.mk に以下のコードを記述する：
+// KEY_OVERRIDE_ENABLE = yes
+//
+// 有効化しない場合は keymap.c のコードをコメントアウトする必要あり
+
+const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_ALT, KC_ESC, KC_GRV);
+const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, KC_TILD);
+const key_override_t *key_overrides[] = {
+  &grave_esc_override,
+  &tilde_esc_override,
+};
+
+// }}}
+
+// -- PERMISSIVE_HOLD_PER_KEY {{{
+//
+// 有効化には config.h に以下のコードを記述する：
+// #define PERMISSIVE_HOLD
+//
+// 有効化しない場合は keymap.c のコードをコメントアウトする必要あり
+
+bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    default:
+      return false;
+  }
+}
+
+// }}}
+
+// -- HOLD_ON_OTHER_KEY_PRESS_PER_KEY {{{
+//
+// 有効化には config.h に以下のコードを記述する：
+// #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
+//
+// 有効化しない場合は keymap.c のコードをコメントアウトする必要あり
+
+bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+
+    // Hold On Other Key Press Mode
+    case NAV_ESC:
+      return true;
+    case SYM_TAB:
+      return true;
+    case HYPR_TAB:
+      return true;
+    case HYPR_BSPC:
+      return true;
+    case CTL_ESC:
+      return true;
+    case CTL_ENT:
+      return true;
+    case SFT_SPC:
+      return true;
+    case SFT_CW:
+      return true;
+    case GUI_LNG2:
+      return true;
+    case RGUI_LNG1:
+      return true;
+
+    // Default Mode
+    case CTL_QUOT:
+      return false;
+    case SFT_BSLS:
+      return false;
+    case CTL_LBRC:
+      return false;
+    case SFT_RBRC:
+      return false;
+    case ALT_LSG_A:
+      return false;
+
+    // Otherwise
+    default:
+      return false;
+
+  }
+}
+
+// }}}
--- keyboards/kbd_firmware/cornelius/keymaps/ichichou/keymap.c
+++ keyboards/kbd_firmware/cornelius/keymaps/ichichou/keymap.c
@@ -16,127 +16,92 @@
 
 #include QMK_KEYBOARD_H
 
-// -- C Lang Macros {{{
+// -- Define Macros {{{
 
 // Modifier Keycodes
 #define RHYPR_T(kc) MT(MOD_RCTL | MOD_RSFT | MOD_RALT | MOD_RGUI, kc)
 #define LCG(kc)     (QK_LCTL | QK_LGUI | (kc))
 
 // Tap-Hold Keys
-#define LOWR_ESC  LT(_LOWER, KC_ESC)
-#define UPPR_TAB  LT(_RAISE, KC_TAB)
-#define FN_GRV    LT(_FN, KC_GRV)
+#define NAV_ESC   LT(_NAV, KC_ESC)
+#define SYM_TAB   LT(_SYM, KC_TAB)
 #define HYPR_TAB  RHYPR_T(KC_TAB)
 #define HYPR_BSPC RHYPR_T(KC_BSPC)
 #define CTL_ESC   CTL_T(KC_ESC)
 #define CTL_ENT   CTL_T(KC_ENT)
 #define CTL_QUOT  CTL_T(KC_QUOT)
 #define CTL_Q     CTL_T(KC_Q)
-#define CTL_LBRC  CTL_T(KC_LBRC)
 #define SFT_SPC   SFT_T(KC_SPC)
-#define SFT_ENT   SFT_T(KC_ENT)
-#define SFT_RBRC  SFT_T(KC_RBRC)
 #define SFT_CW    SFT_T(CW_TOGG)
 #define SFT_BSLS  SFT_T(KC_BSLS)
 #define GUI_LNG2  GUI_T(KC_LNG2)
 #define RGUI_LNG1 RGUI_T(KC_LNG1)
+
+// Home Row Mods
+#define CTL_LBRC  CTL_T(KC_LBRC)
+#define SFT_RBRC  SFT_T(KC_RBRC)
 #define ALT_LSG_A ALT_T(LSG(KC_A))
 
-// -- }}}
+// }}}
 
 // -- Layers {{{
 
 enum layer_names {
   _BASE,
   _MTGAP,
-  _LOWER,
-  _RAISE,
-  _ADJUST,
+  _NAV,
+  _SYM,
+  _TRI,
   _FN,
 };
 
-// -- }}}
-
-// -- Tri Layers {{{
-
+// Tri Layers
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _NAV, _SYM, _TRI);
 }
 
-// -- }}}
-
-// -- Custom Keycodes {{{
-
-// enum my_keycodes {
-//   TEST_1 = SAFE_RANGE,
-// };
-
-// -- }}}
+// }}}
 
 // -- Behavior of Any Keycode {{{
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+
+    // Caps Word / Shift
     case SFT_CW:
       if (record->tap.count && record->event.pressed) {
         caps_word_toggle();
         return false;
       }
       return true;
+
+    // Shift-Command-A / Option
     case ALT_T(LSG(KC_A)):
       if (record->tap.count && record->event.pressed) {
         tap_code16(LSG(KC_A));
         return false;
       }
       return true;
+
+    // Otherwise
     default:
       return true;
+
   }
   return true;
 }
 
-// -- }}}
-
-// -- Combos {{{
-
-// const uint16_t PROGMEM ab_combo[] = {KC_A, KC_B, COMBO_END};
-// combo_t key_combos[] = {
-//   COMBO(ab_combo, KC_ESC),
-// };
-
-// -- }}}
-
-// -- Key Overrides {{{
-
-// const key_override_t grave_esc_override = ko_make_basic(MOD_MASK_ALT, KC_ESC, KC_GRV);
-// const key_override_t tilde_esc_override = ko_make_basic(MOD_MASK_SHIFT, KC_ESC, KC_TILD);
-// const key_override_t *key_overrides[] = {
-//   &grave_esc_override,
-//   &tilde_esc_override,
-// };
-
-// -- }}}
-
-// -- PERMISSIVE_HOLD_PER_KEY {{{
-
-// bool get_permissive_hold(uint16_t keycode, keyrecord_t *record) {
-//   switch (keycode) {
-//     default:
-//       return false;
-//   }
-// }
-
-// -- }}}
+// }}}
 
 // -- HOLD_ON_OTHER_KEY_PRESS_PER_KEY {{{
 
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case LOWR_ESC:
+
+    // Hold On Other Key Press Mode
+    case NAV_ESC:
       return true;
-    case UPPR_TAB:
-      return true;
-    case FN_GRV:
+    case SYM_TAB:
       return true;
     case HYPR_TAB:
       return true;
@@ -148,65 +113,76 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
       return true;
     case SFT_SPC:
       return true;
-    case SFT_ENT:
-      return true;
     case SFT_CW:
-      return true;
-    case SFT_BSLS:
       return true;
     case GUI_LNG2:
       return true;
     case RGUI_LNG1:
       return true;
+
+    // Default Mode
+    case CTL_QUOT:
+      return false;
+    case SFT_BSLS:
+      return false;
+    case CTL_LBRC:
+      return false;
+    case SFT_RBRC:
+      return false;
+    case ALT_LSG_A:
+      return false;
+
+    // Otherwise
     default:
       return false;
+
   }
 }
 
-// -- }}}
+// }}}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT(
-    HYPR_TAB, KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,    KC_Y,    KC_U,     KC_I,      KC_O,    KC_P,    HYPR_BSPC,
-    CTL_ESC,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,    KC_H,    KC_J,     KC_K,      KC_L,    KC_SCLN, CTL_QUOT,
-    SFT_CW,   KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,    KC_N,    KC_M,     KC_COMM,   KC_DOT,  KC_SLSH, SFT_BSLS,
-    MO(_FN),  KC_RCTL, KC_LALT, GUI_LNG2, LOWR_ESC, SFT_SPC, CTL_ENT, UPPR_TAB, RGUI_LNG1, KC_DOWN, KC_UP,   FN_GRV
+    HYPR_TAB, KC_Q,    KC_W,    KC_E,     KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,      KC_O,    KC_P,    HYPR_BSPC,
+    CTL_ESC,  KC_A,    KC_S,    KC_D,     KC_F,    KC_G,       KC_H,    KC_J,    KC_K,      KC_L,    KC_SCLN, CTL_QUOT,
+    SFT_CW,   KC_Z,    KC_X,    KC_C,     KC_V,    KC_B,       KC_N,    KC_M,    KC_COMM,   KC_DOT,  KC_SLSH, SFT_BSLS,
+    MO(_FN),  KC_RCTL, KC_LALT, GUI_LNG2, NAV_ESC, SFT_SPC,    CTL_ENT, SYM_TAB, RGUI_LNG1, KC_DOWN, KC_UP,   MO(_FN)
   ),
 
   [_MTGAP] = LAYOUT(
-    HYPR_TAB, KC_Y,    KC_P,    KC_O,     KC_U,     KC_SCLN, KC_K,    KC_D,     KC_L,      KC_C,    KC_W,  HYPR_BSPC,
-    CTL_ESC,  KC_I,    KC_N,    KC_E,     KC_A,     KC_COMM, KC_M,    KC_H,     KC_T,      KC_S,    KC_R,  CTL_Q,
-    SFT_CW,   KC_Z,    KC_SLSH, KC_QUOT,  KC_DOT,   KC_X,    KC_B,    KC_F,     KC_G,      KC_V,    KC_J,  SFT_BSLS,
-    MO(_FN),  KC_RCTL, KC_LALT, GUI_LNG2, LOWR_ESC, SFT_SPC, CTL_ENT, UPPR_TAB, RGUI_LNG1, KC_DOWN, KC_UP, FN_GRV
+    HYPR_TAB, KC_Y,    KC_P,    KC_O,     KC_U,    KC_SCLN,    KC_K,    KC_D,    KC_L,      KC_C,    KC_W,  HYPR_BSPC,
+    CTL_ESC,  KC_I,    KC_N,    KC_E,     KC_A,    KC_COMM,    KC_M,    KC_H,    KC_T,      KC_S,    KC_R,  CTL_Q,
+    SFT_CW,   KC_Z,    KC_SLSH, KC_QUOT,  KC_DOT,  KC_X,       KC_B,    KC_F,    KC_G,      KC_V,    KC_J,  SFT_BSLS,
+    MO(_FN),  KC_RCTL, KC_LALT, GUI_LNG2, NAV_ESC, SFT_SPC,    CTL_ENT, SYM_TAB, RGUI_LNG1, KC_DOWN, KC_UP, MO(_FN)
   ),
 
-  [_LOWER] = LAYOUT(
-    _______, LSG(KC_S), KC_LPRN,  KC_RPRN,  MEH(KC_C), LSG(KC_T), G(KC_TAB), C(KC_TAB), KC_LCBR, KC_RCBR, G(KC_RBRC), G(KC_UP),
-    _______, ALT_LSG_A, CTL_LBRC, SFT_RBRC, LCG(KC_V), LCG(KC_S), KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT, G(KC_LBRC), G(KC_DOWN),
-    _______, LSG(KC_Z), G(KC_X),  G(KC_C),  LSG(KC_V), G(KC_V),   KC_BSPC,   KC_DEL,    C(KC_A), C(KC_E), XXXXXXX,    XXXXXXX,
-    _______, _______,   _______,  _______,  _______,   _______,   _______,   _______,   _______, _______, _______,    _______
+  [_NAV] = LAYOUT(
+    _______, LSG(KC_S), KC_LPRN,  KC_RPRN,  MEH(KC_C), LSG(KC_T),    G(KC_TAB), C(KC_TAB), KC_LCBR, KC_RCBR, G(KC_RBRC), G(KC_UP),
+    _______, ALT_LSG_A, CTL_LBRC, SFT_RBRC, LCG(KC_V), LCG(KC_S),    KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT, G(KC_LBRC), G(KC_DOWN),
+    _______, LSG(KC_Z), G(KC_X),  G(KC_C),  LSG(KC_V), G(KC_V),      KC_BSPC,   KC_DEL,    C(KC_A), C(KC_E), XXXXXXX,    XXXXXXX,
+    _______, _______,   _______,  _______,  _______,   _______,      _______,   _______,   _______, _______, _______,    _______
   ),
 
-  [_RAISE] = LAYOUT(
-    _______, KC_DOT,  KC_PLUS, KC_UNDS, KC_EXLM, KC_PIPE, KC_GRV,  KC_QUES, KC_CIRC, KC_DLR,  KC_COMM, XXXXXXX,
-    _______, KC_ASTR, KC_EQL,  KC_MINS, KC_0,    KC_AT,   KC_AMPR, KC_1,    KC_PERC, KC_HASH, KC_COLN, KC_QUOT,
-    _______, KC_8,    KC_6,    KC_4,    KC_2,    KC_BSLS, KC_TILD, KC_3,    KC_5,    KC_7,    KC_9,    XXXXXXX,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+  [_SYM] = LAYOUT(
+    _______, KC_DOT,  KC_PLUS, KC_UNDS, KC_EXLM, KC_PIPE,    KC_GRV,  KC_QUES, KC_CIRC, KC_DLR,  KC_COMM, XXXXXXX,
+    _______, KC_ASTR, KC_EQL,  KC_MINS, KC_0,    KC_AT,      KC_AMPR, KC_1,    KC_PERC, KC_HASH, KC_COLN, KC_QUOT,
+    _______, KC_8,    KC_6,    KC_4,    KC_2,    KC_BSLS,    KC_TILD, KC_3,    KC_5,    KC_7,    KC_9,    XXXXXXX,
+    _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______
   ),
 
-  [_ADJUST] = LAYOUT(
-    XXXXXXX, LCA(KC_U),    LCA(KC_I), XXXXXXX,   MEH(KC_C),    C(KC_3), C(KC_6), MEH(KC_C),   A(KC_UP),   XXXXXXX, XXXXXXX, LCA(KC_BSPC),
-    XXXXXXX, LCA(KC_LEFT), LCA(KC_D), LCA(KC_G), LCA(KC_RGHT), C(KC_2), C(KC_5), LCA(KC_C),   A(KC_DOWN), XXXXXXX, XXXXXXX, MEH(KC_ENT),
-    XXXXXXX, LCA(KC_Z),    LCA(KC_X), LCA(KC_V), LCA(KC_B),    C(KC_1), C(KC_4), LCA(KC_ENT), XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX,      XXXXXXX,   XXXXXXX,   _______,      XXXXXXX, XXXXXXX, _______,     XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
+  [_TRI] = LAYOUT(
+    XXXXXXX, LCA(KC_U),    LCA(KC_I), XXXXXXX,   MEH(KC_C),    C(KC_3),    C(KC_6), MEH(KC_C),   A(KC_UP),   XXXXXXX, XXXXXXX, LCA(KC_BSPC),
+    XXXXXXX, LCA(KC_LEFT), LCA(KC_D), LCA(KC_G), LCA(KC_RGHT), C(KC_2),    C(KC_5), LCA(KC_C),   A(KC_DOWN), XXXXXXX, XXXXXXX, MEH(KC_ENT),
+    XXXXXXX, LCA(KC_Z),    LCA(KC_X), LCA(KC_V), LCA(KC_B),    C(KC_1),    C(KC_4), LCA(KC_ENT), XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX,      XXXXXXX,   XXXXXXX,   _______,      XXXXXXX,    XXXXXXX, _______,     XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
   [_FN] = LAYOUT(
-    LCG(KC_Q), KC_F1,   KC_F2,   KC_F3,   KC_F4,   DF(_BASE),  LSG(KC_4), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LCG(KC_Q),
-    XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   DF(_MTGAP), LSG(KC_3), XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY,
-    XXXXXXX,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,    LSG(KC_2), XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE,
-    _______,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    QK_BOOT,   XXXXXXX, XXXXXXX, KC_LEFT, KC_RGHT, _______
+    LCG(KC_Q), KC_F1,   KC_F2,   KC_F3,   KC_F4,   DF(_BASE),     LSG(KC_4), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, LCG(KC_Q),
+    XXXXXXX,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   DF(_MTGAP),    LSG(KC_3), XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY,
+    XXXXXXX,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,       LSG(KC_2), XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE,
+    _______,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       QK_BOOT,   XXXXXXX, XXXXXXX, KC_LEFT, KC_RGHT, _______
   ),
 
 };
