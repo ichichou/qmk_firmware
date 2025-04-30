9886b759deb43478c7459a0c8f44de8bec6a44cb
ichichou <35998815+ichichou@users.noreply.github.com>
2025-04-30 19:20:58 +0900

    Disable combo feature on ichichou keymap

--- keyboards/ai03/equinox_xl/keymaps/ichichou/keymap.c
+++ keyboards/ai03/equinox_xl/keymaps/ichichou/keymap.c
@@ -21,10 +21,12 @@
 #define RHYPR_TAB  RHYPR_T(KC_TAB)
 #define RHYPR_BSPC RHYPR_T(KC_BSPC)
 #define LSFT_SPC   SFT_T(KC_SPC)
-#define LSFT_CW    SFT_T(CW_TOGG)
+#define LSFT_CW    SFT_T(CW_TOGG)  // マクロではなくカスタムキーコードで実装してみる
+#define RSFT_SLSH  RSFT_T(KC_SLSH)
 #define RSFT_BSLS  RSFT_T(KC_BSLS)
 #define LCTL_ESC   CTL_T(KC_ESC)
 #define RCTL_ENT   RCTL_T(KC_ENT)
+#define RCTL_QUOT  RCTL_T(KC_QUOT)
 #define RCTL_TAB   RCTL_T(KC_TAB)
 #define RALT_GRV   RALT_T(KC_GRV)
 #define LGUI_LNG2  GUI_T(KC_LNG2)
@@ -62,8 +64,9 @@ enum layer_names {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
 
-    // TAP:  Caps Word
-    // HOLD: Shift
+    // [Tap] Caps Word, [Hold] Shift
+    //
+    // マクロの上書きではなくカスタムキーコードで実装してみる
     case LSFT_CW:
       if (record->tap.count && record->event.pressed) {
         caps_word_toggle();
@@ -83,10 +86,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // -- Key Overrides {{{
 
-// Key Overrides は Karabiner-Elements との相性が悪いため、併用できない。
+// Key Overrides は Karabiner との相性が悪いため、併用できない。
 // 例えば QMK 側で LCmd-H -> Left とオーバーライドしたなら、Karabiner が干渉して LCmd-H -> LCmd-Left と出力されてしまう。
 // これは Karabiner の Complex Modifications に何もルールを登録していなかったとしても同じである。
-// 唯一の手段として、Key Overrides を使用したいキーボード自体を Karabiner の管理対象から外せば、Karabiner の干渉を免れることができる。
+// 唯一の手段として、Key Overrides を使用したいキーボードを Karabiner の管理対象から外せば、Karabiner の干渉を免れることができる。
+// （当然 Karabiner と Key Overrides の併用はできなくなる）
 
 // const key_override_t lgui_h_override = ko_make_basic(MOD_BIT(KC_LGUI), KC_H, KC_LEFT);
 // const key_override_t lgui_j_override = ko_make_basic(MOD_BIT(KC_LGUI), KC_J, KC_DOWN);
@@ -103,112 +107,107 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // -- Combos {{{
 
-enum combos {
-  SD,
-  KL,
-  WE,
-  IO,
-};
+// コンボは Karabiner の同時押しと干渉するため、すべて Karabiner 側に担当させる。
 
-const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
-const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
-const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
-const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
-
-combo_t key_combos[] = {
-  [SD] = COMBO(sd_combo, MO(_NAV)),
-  [KL] = COMBO(kl_combo, MO(_NAV)),
-  [WE] = COMBO(we_combo, MO(_WIN)),
-  [IO] = COMBO(io_combo, MO(_WIN)),
-};
+// enum combos {
+//   SD,
+//   KL,
+//   WE,
+//   IO,
+// };
+//
+// const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
+// const uint16_t PROGMEM kl_combo[] = {KC_K, KC_L, COMBO_END};
+// const uint16_t PROGMEM we_combo[] = {KC_W, KC_E, COMBO_END};
+// const uint16_t PROGMEM io_combo[] = {KC_I, KC_O, COMBO_END};
+//
+// combo_t key_combos[] = {
+//   [SD] = COMBO(sd_combo, MO(_NAV)),
+//   [KL] = COMBO(kl_combo, MO(_NAV)),
+//   [WE] = COMBO(we_combo, MO(_WIN)),
+//   [IO] = COMBO(io_combo, MO(_WIN)),
+// };
 
 // }}}
 
 // -- Combo Configuration {{{
 
-// COMBO_TERM_PER_COMBO (Default: 50)
-// COMBO_TERM を50にすると、Karabiner での同時押し判定に支障が出る。
-uint16_t get_combo_term(uint16_t index, combo_t *combo) {
-  switch (index) {
-    case SD:
-      return 20;
-    case KL:
-      return 20;
-    case WE:
-      return 20;
-    case IO:
-      return 20;
-  }
-  return COMBO_TERM;
-}
-
-// COMBO_MUST_TAP_PER_COMBO
-bool get_combo_must_tap(uint16_t index, combo_t *combo) {
-  switch (index) {
-  }
-  return false;
-}
-
-// COMBO_MUST_HOLD_PER_COMBO
-bool get_combo_must_hold(uint16_t index, combo_t *combo) {
-  switch (index) {
-    case SD:
-      return true;
-    case KL:
-      return true;
-    case WE:
-      return true;
-    case IO:
-      return true;
-  }
-  return false;
-}
+// // COMBO_TERM_PER_COMBO (Default: 50)
+// //
+// // Karabiner 側の閾値が 50 ms の場合、COMBO_TERM は 20 ms 程度でないと、Karabiner での同時押し判定に支障が出る。
+// // Karabiner 側の閾値が 70 ms なら、COMBO_TERM を 50 ms にしても実用に耐える。
+// // 新下駄配列の同時押しに絡まないキーでのコンボなら関係ないと思われる。
+// uint16_t get_combo_term(uint16_t index, combo_t *combo) {
+//   switch (index) {
+//     case SD: return 20;
+//     case KL: return 20;
+//     case WE: return 20;
+//     case IO: return 20;
+//   }
+//   return COMBO_TERM;
+// }
+//
+// // COMBO_MUST_TAP_PER_COMBO
+// bool get_combo_must_tap(uint16_t index, combo_t *combo) {
+//   switch (index) {
+//   }
+//   return false;
+// }
+//
+// // COMBO_MUST_HOLD_PER_COMBO
+// bool get_combo_must_hold(uint16_t index, combo_t *combo) {
+//   switch (index) {
+//     case SD: return true;
+//     case KL: return true;
+//     case WE: return true;
+//     case IO: return true;
+//   }
+//   return false;
+// }
 
 // }}}
 
 // -- Tap-Hold Configuration {{{
+
+// 'Hold on other key press' と 'permissive hold' を併用するには、
+// HOLD_ON_OTHER_KEY_PRESS_PER_KEY と PERMISSIVE_HOLD_PER_KEY を設定することが必要。
+//
+// HOLD_ON_OTHER_KEY_PRESS と PERMISSIVE_HOLD_PER_KEY の組み合わせはうまく動作しない。
+// Tap-hold キーの基本動作を 'hold on other key press' としながら、
+// 個別に指定したキーを 'permissive hold' とすることを意図しているが、
+// 指定しても 'permissive hold' とはならず、すべて 'hold on other key press' になってしまう。
 
 // Hold On Other Key Press Mode
 bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
 
     // -- Layer Keys
-    case NAV_ESC:
-      return true;
-    case SYM_TAB:
-      return true;
-    case SYM_ENT:
-      return true;
-    case FN_GRV:
-      return true;
+    case NAV_ESC: return true;
+    case NAV_TAB: return true;
+    case SYM_TAB: return true;
+    case SYM_ENT: return true;
+    case FN_BSLS: return true;
+    case FN_GRV:  return true;
 
     // -- Mod Keys
-    case RHYPR_TAB:
-      return true;
-    case RHYPR_BSPC:
-      return true;
-    case LSFT_SPC:
-      return true;
-    case LSFT_CW:
-      return true;
-    case RSFT_BSLS:
-      return true;
-    case LCTL_ESC:
-      return true;
-    case RCTL_ENT:
-      return true;
-    case LGUI_LNG2:
-      return true;
-    case RGUI_LNG1:
-      return true;
+    case RHYPR_TAB:  return true;
+    case RHYPR_BSPC: return true;
+    case LSFT_SPC:   return true;
+    case LSFT_CW:    return true;
+    case RSFT_SLSH:  return true;
+    case RSFT_BSLS:  return true;
+    case LCTL_ESC:   return true;
+    case RCTL_ENT:   return true;
+    case RCTL_QUOT:  return true;
+    case RCTL_TAB:   return true;
+    case RALT_GRV:   return true;
+    case LGUI_LNG2:  return true;
+    case RGUI_LNG1:  return true;
 
     // -- Home Row Mods
-    case LCTL_LBRC:
-      return true;
-    case LSFT_RBRC:
-      return true;
-    case LGUI_GRV:
-      return true;
+    case LCTL_LBRC: return true;
+    case LSFT_RBRC: return true;
+    case LGUI_GRV:  return true;
 
     // Otherwise
     default:
@@ -254,17 +253,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_WIN] = LAYOUT(
-    XXXXXXX,          LCA(KC_U),    LCA(KC_I), XXXXXXX,   MEH(KC_C),    C(KC_1), XXXXXXX, C(KC_4), MEH(KC_C),   A(KC_UP),   XXXXXXX, XXXXXXX, LCA(KC_BSPC), XXXXXXX,
-    XXXXXXX,          LCA(KC_LEFT), LCA(KC_D), LCA(KC_G), LCA(KC_RGHT), C(KC_2), XXXXXXX, C(KC_5), LCA(KC_C),   A(KC_DOWN), XXXXXXX, XXXXXXX,               MEH(KC_ENT),
-    XXXXXXX, XXXXXXX, LCA(KC_Z),    LCA(KC_X), LCA(KC_V), LCA(KC_B),    C(KC_3), XXXXXXX, C(KC_6), LCA(KC_ENT), LCG(KC_F),  XXXXXXX, XXXXXXX,               XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,               XXXXXXX,          XXXXXXX,                          XXXXXXX, XXXXXXX,      XXXXXXX
+    XXXXXXX,          LCA(KC_U),    LCA(KC_I), XXXXXXX,   MEH(KC_C),    C(KC_1), XXXXXXX, C(KC_4), MEH(KC_C),   A(KC_UP),    XXXXXXX, XXXXXXX, LCA(KC_BSPC), XXXXXXX,
+    XXXXXXX,          LCA(KC_LEFT), XXXXXXX,   XXXXXXX,   LCA(KC_RGHT), C(KC_2), XXXXXXX, C(KC_5), LCA(KC_C),   A(KC_DOWN),  XXXXXXX, XXXXXXX,               MEH(KC_ENT),
+    XXXXXXX, XXXXXXX, LCA(KC_Z),    LCA(KC_X), LCA(KC_V), LCA(KC_B),    C(KC_3), XXXXXXX, C(KC_6), LCA(KC_ENT), MEH(KC_ENT), XXXXXXX, XXXXXXX,               XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX,               XXXXXXX,          XXXXXXX,                           XXXXXXX, XXXXXXX,      XXXXXXX
   ),
 
   [_FN] = LAYOUT(
     LCG(KC_Q),          KC_F1, KC_F2,  KC_F3,  KC_F4,  XXXXXXX, XXXXXXX, LSG(KC_2), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MNXT,
     XXXXXXX,            KC_F5, KC_F6,  KC_F7,  KC_F8,  XXXXXXX, XXXXXXX, LSG(KC_3), XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV,          KC_MPLY,
     XXXXXXX,   XXXXXXX, KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX, XXXXXXX, LSG(KC_4), XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE,          _______,
-    _______,   XXXXXXX, XXXXXXX,               XXXXXXX,         QK_BOOT,            XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
+    _______,   XXXXXXX, XXXXXXX,               EE_CLR,          QK_BOOT,            XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
 };
