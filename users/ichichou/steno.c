cea678088e782b97a3ed262fe08e6a7fe1005471
ichichou <35998815+ichichou@users.noreply.github.com>
2025-05-30 15:48:56 +0900

    Update steno config (nkro)

--- users/ichichou/steno.c
+++ users/ichichou/steno.c
@@ -8,7 +8,9 @@ bool process_record_steno(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         layer_move(_BASE);
         set_single_default_layer(_BASE);
-        keymap_config.nkro = false;
+        #if NKRO_DEFAULT_ON != true
+          keymap_config.nkro = false;
+        #endif
       }
       return false;
 
@@ -16,7 +18,9 @@ bool process_record_steno(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         layer_move(_BASE);
         layer_on(_PLOVER);
-        keymap_config.nkro = true;
+        #if NKRO_DEFAULT_ON != true
+          keymap_config.nkro = true;
+        #endif
       }
       return false;
 
@@ -24,21 +28,27 @@ bool process_record_steno(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         layer_move(_BASE);
         layer_on(_GEMINI);
-        keymap_config.nkro = true;
+        #if NKRO_DEFAULT_ON != true
+          keymap_config.nkro = true;
+        #endif
       }
       return false;
 
     case EXT_PLOVER:
       if (record->event.pressed) {
         layer_off(_PLOVER);
-        keymap_config.nkro = false;
+        #if NKRO_DEFAULT_ON != true
+          keymap_config.nkro = false;
+        #endif
       }
       return false;
 
     case EXT_GEMINI:
       if (record->event.pressed) {
         layer_off(_GEMINI);
-        keymap_config.nkro = false;
+        #if NKRO_DEFAULT_ON != true
+          keymap_config.nkro = false;
+        #endif
       }
       return false;
 
