7f790c448ca569b57f742cb1682945d6bdae743e
ichichou <35998815+ichichou@users.noreply.github.com>
2025-06-01 23:02:17 +0900

    Update keymap for The Uni

--- keyboards/stenokeyboards/the_uni/keymaps/ichichou/config.h
+++ keyboards/stenokeyboards/the_uni/keymaps/ichichou/config.h
@@ -1,2 +1,1 @@
-#define DYNAMIC_KEYMAP_LAYER_COUNT 3
 #define NKRO_DEFAULT_ON true
--- keyboards/stenokeyboards/the_uni/keymaps/ichichou/rules.mk
+++ keyboards/stenokeyboards/the_uni/keymaps/ichichou/rules.mk
@@ -1,6 +1,5 @@
 USER_NAME := no_user
 
-VIA_ENABLE = yes
 NKRO_ENABLE = yes
 STENO_ENABLE = yes
 STENO_PROTOCOL = geminipr
