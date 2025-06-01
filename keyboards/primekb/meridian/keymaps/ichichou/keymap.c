dffbe5dacb76317dc0c1004920e4b958aebea2ba
ichichou <35998815+ichichou@users.noreply.github.com>
2025-06-01 15:08:32 +0900

    Update keymap for Meridian

--- keyboards/primekb/meridian/keymaps/ichichou/keymap.c
+++ keyboards/primekb/meridian/keymaps/ichichou/keymap.c
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 void keyboard_post_init_user(void) {
   rgblight_disable();
 }
-// これで RGB ライトがオフになっているか要確認
+// これで RGB ライトがオフになるか要確認
 
 #define LAYOUT_wrapper(...) LAYOUT_split_bs_rshift(__VA_ARGS__)
 
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,    __________________1_TO_5___________________, KC_6,   __________________7_TO_MINS________________, KC_EQL,  KC_BSPC, KC_DEL,
     RHYPR_TAB, __________________BASE_L1__________________,         __________________BASE_L1__________________, KC_LBRC, KC_RBRC, KC_BSLS,
     LCTL_ESC,  __________________BASE_L2__________________,         __________________BASE_L2__________________, KC_QUOT,          RCTL_ENT,
-    LSFT_CW,   __________________BASE_L3__________________, KC_GRV, __________________BASE_L3__________________,          KC_UP,   FN_BSLS,
+    LSFT_CW,   __________________BASE_L3__________________, KC_GRV, __________________BASE_L3__________________,          KC_UP,   FN,
     FN,        KC_LALT,                LGUI_LNG2, LSFT_SPC,         SYM_ENT, RGUI_LNG1,                          KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
@@ -70,15 +70,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, __________________WIN_L1___________________,          __________________WIN_R1___________________, XXXXXXX, XXXXXXX,      MEH(KC_ENT),
     XXXXXXX, __________________WIN_L2___________________,          __________________WIN_R2___________________, XXXXXXX,               XXXXXXX,
     XXXXXXX, __________________WIN_L3___________________, XXXXXXX, __________________WIN_R3___________________,          XXXXXXX,      XXXXXXX,
-    XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX,          _______, XXXXXXX,                            XXXXXXX, XXXXXXX,      XXXXXXX
+    XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX,      XXXXXXX
   ),
 
   [_FN] = LAYOUT_wrapper(
     OS_LOCK, __________________F1_TO_F5_________________, KC_F6,   __________________F7_TO_F11________________, KC_F12,  OS_LOCK, OS_LOCK,
     XXXXXXX, __________________FN_L1____________________,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD, KC_GRV,
-    XXXXXXX, __________________FN_L2____________________,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT,          KC_MPLY,
-    XXXXXXX, __________________FN_L3____________________, QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU,          KC_MUTE, _______,
-    _______, XXXXXXX,                    XXXXXXX, EE_CLR,          XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX
+    DM_REC1, __________________FN_L2____________________,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT,          KC_MPLY,
+    DM_PLY1, __________________FN_L3____________________, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU,          KC_MUTE, _______,
+    _______, XXXXXXX,                    XXXXXXX, EE_CLR,          QK_BOOT, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
 };
