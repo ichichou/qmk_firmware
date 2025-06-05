11beae32fe8b280c0a337a990b85c810ff762475
ichichou <35998815+ichichou@users.noreply.github.com>
2025-06-05 10:37:58 +0900

    Fix keymap

--- keyboards/primekb/meridian/keymaps/ichichou/keymap.c
+++ keyboards/primekb/meridian/keymaps/ichichou/keymap.c
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_wrapper(
     KC_ESC,    __________________1_TO_5___________________, KC_6,   __________________7_TO_MINS________________, KC_EQL,  KC_BSPC, KC_DEL,
-    RHYPR_TAB, __________________BASE_L1__________________,         __________________BASE_L1__________________, KC_LBRC, KC_RBRC, KC_BSLS,
-    LCTL_ESC,  __________________BASE_L2__________________,         __________________BASE_L2__________________, KC_QUOT,          RCTL_ENT,
-    LSFT_CW,   __________________BASE_L3__________________, KC_GRV, __________________BASE_L3__________________,          KC_UP,   FN,
+    RHYPR_TAB, __________________BASE_L1__________________,         __________________BASE_R1__________________, KC_LBRC, KC_RBRC, KC_BSLS,
+    LCTL_ESC,  __________________BASE_L2__________________,         __________________BASE_R2__________________, KC_QUOT,          RCTL_ENT,
+    LSFT_CW,   __________________BASE_L3__________________, KC_GRV, __________________BASE_R3__________________,          KC_UP,   FN,
     FN,        KC_LALT,                LGUI_LNG2, LSFT_SPC,         SYM_ENT, RGUI_LNG1,                          KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
