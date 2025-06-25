fa7eb8b30d643aa6c7f81375fc90757d8e2da1bd
ichichou <35998815+ichichou@users.noreply.github.com>
2025-06-25 16:06:27 +0900

    Update wrappers

--- users/ichichou/ichichou.h
+++ users/ichichou/ichichou.h
@@ -484,58 +484,66 @@ enum my_keycodes {
 #define __________________BASE_L2__________________ KC_A, KC_S, KC_D, KC_F, KC_G
 #define __________________BASE_L3__________________ KC_Z, KC_X, KC_C, KC_V, KC_B
 
-#define __________________BASE_R1__________________ KC_Y, KC_U, KC_I,    KC_O,   KC_P
-#define __________________BASE_R2__________________ KC_H, KC_J, KC_K,    KC_L,   KC_SCLN
-#define __________________BASE_R3__________________ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH
+#define __________________BASE_R1__________________ KC_Y, KC_U, KC_I,    KC_O,   KC_P     // KC_BSPC | KC_LBRC
+#define __________________BASE_R2__________________ KC_H, KC_J, KC_K,    KC_L,   KC_SCLN  // KC_QUOT
+#define __________________BASE_R3__________________ KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH  // FN      | KC_RSFT
 
-#define __________________NAV_L1___________________ LSG(KC_C), KC_LPRN,   KC_RPRN,   C(KC_TAB), LSG(KC_T)
+#define __________________NAV_L1___________________ LSG(KC_C), KC_LPRN,   KC_RPRN,   S(KC_SPC), LSG(KC_T)
 #define __________________NAV_L2___________________ LSG(KC_A), LCTL_LBRC, LSFT_RBRC, LCG(KC_V), LCG(KC_S)
 #define __________________NAV_L3___________________ LSG(KC_Z), G(KC_X),   G(KC_C),   LSG(KC_V), G(KC_V)
 
-#define __________________NAV_R1___________________ G(KC_TAB), C(KC_TAB), KC_LCBR, KC_RCBR, G(KC_RBRC)
-#define __________________NAV_R2___________________ KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT, G(KC_LBRC)
-#define __________________NAV_R3___________________ KC_BSPC,   KC_DEL,    C(KC_A), C(KC_E), XXXXXXX
+#define __________________NAV_R1___________________ G(KC_TAB), C(KC_TAB), KC_LCBR, KC_RCBR, G(KC_RBRC)  // G(KC_UP)
+#define __________________NAV_R2___________________ KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT, G(KC_LBRC)  // G(KC_DOWN)
+#define __________________NAV_R3___________________ KC_BSPC,   KC_DEL,    C(KC_A), C(KC_E), XXXXXXX     // XXXXXXX
 
 #define __________________SYM_L1___________________ KC_DOT,  KC_PLUS, KC_UNDS, KC_EXLM, KC_PIPE
 #define __________________SYM_L2___________________ KC_ASTR, KC_EQL,  KC_MINS, KC_0,    KC_AT
 #define __________________SYM_L3___________________ KC_8,    KC_6,    KC_4,    KC_2,    KC_BSLS
 
-#define __________________SYM_R1___________________ KC_COMM, KC_AMPR, KC_CIRC, KC_DLR,  KC_LPRN
-#define __________________SYM_R2___________________ KC_TILD, KC_1,    KC_PERC, KC_HASH, KC_LBRC
-#define __________________SYM_R3___________________ KC_GRV,  KC_3,    KC_5,    KC_7,    KC_9
+#define __________________SYM_R1___________________ XXXXXXX, KC_AMPR, KC_CIRC, KC_DLR,  KC_COMM  // XXXXXXX
+#define __________________SYM_R2___________________ KC_TILD, KC_1,    KC_PERC, KC_HASH, KC_COLN  // XXXXXXX
+#define __________________SYM_R3___________________ KC_GRV,  KC_3,    KC_5,    KC_7,    KC_9     // XXXXXXX
+
+#define __________________SYM_R1_WITHOUT_NAV_______ KC_COMM, KC_AMPR, KC_CIRC, KC_DLR,  KC_LPRN  // KC_RPRN
+#define __________________SYM_R2_WITHOUT_NAV_______ KC_TILD, KC_1,    KC_PERC, KC_HASH, KC_LBRC  // KC_RBRC
+#define __________________SYM_R3_WITHOUT_NAV_______ KC_GRV,  KC_3,    KC_5,    KC_7,    KC_9     // XXXXXXX
 
 #define __________________WIN_L1___________________ XXXXXXX,      LCA(KC_U), LCA(KC_I), MEH(KC_C),    C(KC_1)
 #define __________________WIN_L2___________________ LCA(KC_LEFT), LCA(KC_J), LCA(KC_K), LCA(KC_RGHT), C(KC_2)
 #define __________________WIN_L3___________________ LCA(KC_Z),    LCA(KC_X), LCA(KC_V), LCA(KC_B),    C(KC_3)
 
-#define __________________WIN_R1___________________ C(KC_4), MEH(KC_C),   A(KC_UP),    XXXXXXX, XXXXXXX
-#define __________________WIN_R2___________________ C(KC_5), LCA(KC_C),   A(KC_DOWN),  XXXXXXX, XXXXXXX
-#define __________________WIN_R3___________________ C(KC_6), LCA(KC_ENT), MEH(KC_ENT), XXXXXXX, XXXXXXX
+#define __________________WIN_R1___________________ C(KC_4), MEH(KC_C),   A(KC_UP),    XXXXXXX, XXXXXXX  // LCA(KC_BSPC)
+#define __________________WIN_R2___________________ C(KC_5), LCA(KC_C),   A(KC_DOWN),  XXXXXXX, XXXXXXX  // MEH(KC_ENT)
+#define __________________WIN_R3___________________ C(KC_6), LCA(KC_ENT), MEH(KC_ENT), XXXXXXX, XXXXXXX  // XXXXXXX
 
 #define __________________FN_L1____________________ KC_F1, KC_F2,  KC_F3,  KC_F4,  XXXXXXX
 #define __________________FN_L2____________________ KC_F5, KC_F6,  KC_F7,  KC_F8,  XXXXXXX
 #define __________________FN_L3____________________ KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX
 
-#define __________________FN_R1____________________ LSG(KC_2), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define __________________FN_R2____________________ LSG(KC_3), XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT
-#define __________________FN_R3____________________ LSG(KC_4), XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU
-#define __________________FN_R3_LEFT_SHIFTED_______ LSG(KC_4), XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE
+#define __________________FN_R1____________________ LSG(KC_2), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  // OS_LOCK | XXXXXXX
+#define __________________FN_R2____________________ LSG(KC_3), XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT  // KC_MPLY
+#define __________________FN_R3____________________ LSG(KC_4), XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU  // KC_MUTE
 
+#define __________________FN_R3_LEFT_SHIFTED_______ LSG(KC_4), XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE  // FN
+
+// Number Row
 #define __________________1_TO_5___________________ KC_1, KC_2, KC_3, KC_4, KC_5
 #define __________________6_TO_0___________________ KC_6, KC_7, KC_8, KC_9, KC_0
 #define __________________7_TO_MINS________________ KC_7, KC_8, KC_9, KC_0, KC_MINS
 
+// F-Key Row
 #define __________________F1_TO_F5_________________ KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5
 #define __________________F6_TO_F10________________ KC_F6, KC_F7, KC_F8, KC_F9,  KC_F10
 #define __________________F7_TO_F11________________ KC_F7, KC_F8, KC_F9, KC_F10, KC_F11
 
 #define __________________NONE_____________________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 
-#define __________________F1_TO_F4________ KC_F1, KC_F2,  KC_F3,  KC_F4
-#define __________________F5_TO_F8________ KC_F5, KC_F6,  KC_F7,  KC_F8
-#define __________________F9_TO_F12_______ KC_F9, KC_F10, KC_F11, KC_F12
+// A Part of FN
+#define __________________F1_TO_F4________          KC_F1, KC_F2,  KC_F3,  KC_F4
+#define __________________F5_TO_F8________          KC_F5, KC_F6,  KC_F7,  KC_F8
+#define __________________F9_TO_F12_______          KC_F9, KC_F10, KC_F11, KC_F12
 
-#define __________________NONE____________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define __________________NONE____________          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 
 // }}}
 
