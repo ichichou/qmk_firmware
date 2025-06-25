c1e28943e9911a77e7d6c8d3680eb97f69087eb1
ichichou <35998815+ichichou@users.noreply.github.com>
2025-06-25 16:06:47 +0900

    Update keymap for Altair-X

--- keyboards/ai03/altair_x/keymaps/ichichou/keymap.c
+++ keyboards/ai03/altair_x/keymaps/ichichou/keymap.c
@@ -37,24 +37,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_SYM] = LAYOUT_wrapper(
-    _______, __________________SYM_L1___________________, XXXXXXX, XXXXXXX, __________________SYM_R1___________________, KC_RPRN,
-    _______, __________________SYM_L2___________________, XXXXXXX, XXXXXXX, __________________SYM_R2___________________, KC_RBRC,
+    _______, __________________SYM_L1___________________, XXXXXXX, XXXXXXX, __________________SYM_R1___________________, XXXXXXX,
+    _______, __________________SYM_L2___________________, XXXXXXX, XXXXXXX, __________________SYM_R2___________________, XXXXXXX,
     _______, __________________SYM_L3___________________, XXXXXXX, XXXXXXX, __________________SYM_R3___________________, XXXXXXX,
                                _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_WIN] = LAYOUT_wrapper(
-    XXXXXXX, __________________WIN_L1___________________, XXXXXXX, XXXXXXX, __________________WIN_R1___________________, LCA(KC_BSPC),
-    XXXXXXX, __________________WIN_L2___________________, XXXXXXX, XXXXXXX, __________________WIN_R2___________________, MEH(KC_ENT),
-    XXXXXXX, __________________WIN_L3___________________, XXXXXXX, XXXXXXX, __________________WIN_R3___________________, XXXXXXX,
+    XXXXXXX, __________________WIN_L1___________________, C(KC_1), C(KC_4), __________________WIN_R1___________________, LCA(KC_BSPC),
+    XXXXXXX, __________________WIN_L2___________________, C(KC_2), C(KC_5), __________________WIN_R2___________________, MEH(KC_ENT),
+    XXXXXXX, __________________WIN_L3___________________, C(KC_3), C(KC_6), __________________WIN_R3___________________, XXXXXXX,
                                XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX
   ),
 
   [_FN] = LAYOUT_wrapper(
-    OS_LOCK, __________________FN_R1____________________, BASE,    LSG(KC_2), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, OS_LOCK,
-    DM_REC1, __________________FN_R2____________________, MTGAP,   LSG(KC_3), XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV, KC_MNXT, KC_MPLY,
-    DM_PLY1, __________________FN_R3____________________, XXXXXXX, LSG(KC_4), XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE, _______,
-                                _______, XXXXXXX, EE_CLR, QK_BOOT, QK_BOOT,   EE_CLR, XXXXXXX, XXXXXXX
+    OS_LOCK, __________________FN_R1____________________, BASE,    LSG(KC_2), __________________FN_L1____________________, OS_LOCK,
+    DM_REC1, __________________FN_R2____________________, MTGAP,   LSG(KC_3), __________________FN_L2____________________, KC_MPLY,
+    DM_PLY1, __________________FN_R3____________________, XXXXXXX, LSG(KC_4), __________________FN_R3_LEFT_SHIFTED_______, _______,
+                                _______, XXXXXXX, EE_CLR, QK_BOOT, QK_BOOT,   XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
 };
