2ae3c6d2998b1210896084604905c7ac3e7e7748
ichichou <35998815+ichichou@users.noreply.github.com>
2025-12-28 13:00:18 +0900

    Update Altair-X keymap

--- keyboards/ai03/altair_x/keymaps/ichichou/keymap.c
+++ keyboards/ai03/altair_x/keymaps/ichichou/keymap.c
@@ -16,30 +16,30 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_wrapper(
-    RHYPR_TAB, __________________BASE_L1__________________, G(KC_LBRC),    G(KC_RBRC),    __________________BASE_R1__________________, KC_BSPC,
-    LCTL_ESC,  __________________BASE_L2__________________, G(KC_RBRC),    G(KC_LBRC),    __________________BASE_R2__________________, KC_QUOT,
-    KC_LSFT,   __________________BASE_L3__________________, RGUI(KC_LALT), RGUI(KC_RALT), __________________BASE_R3__________________, FN_BSLS,
-                                    FN, LGUI_LNG2, NAV_ESC, LSFT_SPC,      RCTL_ENT,      SYM_TAB, RGUI_LNG1, RALT_GRV
+    RHYPR_TAB, __________________BASE_L1__________________, G(KC_LBRC), XXXXXXX,  __________________BASE_R1__________________, KC_BSPC,
+    LCTL_ESC,  __________________BASE_L2__________________, G(KC_RBRC), XXXXXXX,  __________________BASE_R2__________________, KC_QUOT,
+    KC_LSFT,   __________________BASE_L3__________________, FN,         FN,       __________________BASE_R3__________________, KC_BSLS,
+                               KC_LALT, LGUI_LNG2, NAV_ESC, LSFT_SPC,   RCTL_ENT, SYM_TAB, RGUI_LNG1, FN_GRV
   ),
 
   [_MTGAP] = LAYOUT_wrapper(
-    RHYPR_TAB, __________________MTGAP_L1_________________, G(KC_LBRC),    G(KC_RBRC),    __________________MTGAP_R1_________________, KC_BSPC,
-    LCTL_ESC,  __________________MTGAP_L2_________________, G(KC_RBRC),    G(KC_LBRC),    __________________MTGAP_R2_________________, MT_QUOT,
-    KC_LSFT,   __________________MTGAP_L3_________________, RGUI(KC_LALT), RGUI(KC_RALT), __________________MTGAP_R3_________________, FN_BSLS,
-                                    FN, LGUI_LNG2, NAV_ESC, LSFT_SPC,      RCTL_ENT,      SYM_TAB, RGUI_LNG1, RALT_GRV
+    RHYPR_TAB, __________________MTGAP_L1_________________, G(KC_LBRC), XXXXXXX,  __________________MTGAP_R1_________________, KC_BSPC,
+    LCTL_ESC,  __________________MTGAP_L2_________________, G(KC_RBRC), XXXXXXX,  __________________MTGAP_R2_________________, MT_QUOT,
+    KC_LSFT,   __________________MTGAP_L3_________________, FN,         FN,       __________________MTGAP_R3_________________, KC_BSLS,
+                               KC_LALT, LGUI_LNG2, NAV_ESC, LSFT_SPC,   RCTL_ENT, SYM_TAB, RGUI_LNG1, FN_GRV
   ),
 
   [_NAV] = LAYOUT_wrapper(
     _______, __________________NAV_L1___________________, XXXXXXX, XXXXXXX, __________________NAV_R1___________________, G(KC_UP),
     _______, __________________NAV_L2___________________, XXXXXXX, XXXXXXX, __________________NAV_R2___________________, G(KC_DOWN),
-    _______, __________________NAV_L3___________________, XXXXXXX, XXXXXXX, __________________NAV_R3___________________, XXXXXXX,
+    _______, __________________NAV_L3___________________, _______, _______, __________________NAV_R3___________________, XXXXXXX,
                                _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
   [_SYM] = LAYOUT_wrapper(
     _______, __________________SYM_L1___________________, XXXXXXX, XXXXXXX, __________________SYM_R1___________________, XXXXXXX,
     _______, __________________SYM_L2___________________, XXXXXXX, XXXXXXX, __________________SYM_R2___________________, XXXXXXX,
-    _______, __________________SYM_L3___________________, XXXXXXX, XXXXXXX, __________________SYM_R3___________________, XXXXXXX,
+    _______, __________________SYM_L3___________________, _______, _______, __________________SYM_R3___________________, XXXXXXX,
                                _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
@@ -53,8 +53,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_FN] = LAYOUT_wrapper(
     OS_LOCK, __________________FN_L1____________________, BASE,    LSG(KC_2), __________________FN_R1____________________, OS_LOCK,
     XXXXXXX, __________________FN_L2____________________, MTGAP,   LSG(KC_3), __________________FN_R2____________________, KC_MPLY,
-    XXXXXXX, __________________FN_L3____________________, XXXXXXX, LSG(KC_4), __________________FN_R3_LEFT_SHIFTED_______, _______,
-                                _______, XXXXXXX, EE_CLR, QK_BOOT, QK_BOOT,   XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, __________________FN_L3____________________, _______, LSG(KC_4), __________________FN_R3____________________, KC_MUTE,
+                                XXXXXXX, XXXXXXX, EE_CLR, QK_BOOT, QK_BOOT,   EE_CLR, XXXXXXX, _______
   ),
 
 };
