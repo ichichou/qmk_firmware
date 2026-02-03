e7e178cfb8d98d181289bc774c784fbad5d1e0eb
ichichou <35998815+ichichou@users.noreply.github.com>
2026-02-03 23:38:54 +0900

    Update Equinox XL keymap

--- keyboards/ai03/equinox_xl/keymaps/ichichou/keymap.c
+++ keyboards/ai03/equinox_xl/keymaps/ichichou/keymap.c
@@ -10,16 +10,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_wrapper(
     RHYPR_TAB,     __________________BASE_L1__________________, KC_LBRC, __________________BASE_R1__________________, KC_BSPC, KC_QUOT,
-    LCTL_ESC,      __________________BASE_L2__________________, KC_RBRC, __________________BASE_R2__________________,          RCTL_ENT,
-    LSFT_CW,  NAV, __________________BASE_L3__________________, KC_GRV,  __________________BASE_R3__________________,          FN_BSLS,
-    FN, XXXXXXX, LGUI_LNG2,                           LSFT_SPC, RCTL_TAB, SYM_ENT,                         RGUI_LNG1, XXXXXXX, RALT_GRV
+    LCTL_ESC,      __________________BASE_L2__________________, KC_RBRC, __________________BASE_R2__________________,          KC_ENT,
+    KC_LSFT,  NAV, __________________BASE_L3__________________, KC_BSLS, __________________BASE_R3__________________,          FN_BSLS,
+    KC_LALT, XXXXXXX, LGUI_LNG2,                      LSFT_SPC, KC_RCTL, SYM_ENT,                          RGUI_LNG1, XXXXXXX, FN_GRV
   ),
 
   [_MTGAP] = LAYOUT_wrapper(
     RHYPR_TAB,     __________________MTGAP_L1_________________, KC_LBRC, __________________MTGAP_R1_________________, KC_BSPC, MT_QUOT,
-    LCTL_ESC,      __________________MTGAP_L2_________________, KC_RBRC, __________________MTGAP_R2_________________,          RCTL_ENT,
-    LSFT_CW,  NAV, __________________MTGAP_L3_________________, KC_GRV,  __________________MTGAP_R3_________________,          FN_BSLS,
-    FN, XXXXXXX, LGUI_LNG2,                           LSFT_SPC, RCTL_TAB, SYM_ENT,                         RGUI_LNG1, XXXXXXX, RALT_GRV
+    LCTL_ESC,      __________________MTGAP_L2_________________, KC_RBRC, __________________MTGAP_R2_________________,          KC_ENT,
+    KC_LSFT,  NAV, __________________MTGAP_L3_________________, KC_BSLS, __________________MTGAP_R3_________________,          FN_BSLS,
+    KC_LALT, XXXXXXX, LGUI_LNG2,                      LSFT_SPC, KC_RCTL, SYM_ENT,                          RGUI_LNG1, XXXXXXX, FN_GRV
   ),
 
   [_NAV] = LAYOUT_wrapper(
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,          __________________WIN_L1___________________, XXXXXXX, __________________WIN_R1___________________, LCA(KC_BSPC), XXXXXXX,
     XXXXXXX,          __________________WIN_L2___________________, XXXXXXX, __________________WIN_R2___________________,               MEH(KC_ENT),
     XXXXXXX, _______, __________________WIN_L3___________________, XXXXXXX, __________________WIN_R3___________________,               XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, _______,                            XXXXXXX, XXXXXXX,      XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX,      XXXXXXX
   ),
 
   [_FN] = LAYOUT_wrapper(
     OS_LOCK,          __________________FN_L1____________________, BASE,    LSG(KC_2), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MNXT,
-    DM_REC1,          __________________FN_L2____________________, MTGAP,   LSG(KC_3), XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV,          KC_MPLY,
-    DM_PLY1, XXXXXXX, __________________FN_L3____________________, XXXXXXX, LSG(KC_4), XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE,          _______,
-    _______, XXXXXXX, XXXXXXX,                             EE_CLR, QK_BOOT, XXXXXXX,                              XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX,          __________________FN_L2____________________, MTGAP,   LSG(KC_3), XXXXXXX, XXXXXXX, XXXXXXX, KC_MPRV,          KC_MPLY,
+    XXXXXXX, XXXXXXX, __________________FN_L3____________________, XXXXXXX, LSG(KC_4), XXXXXXX, KC_VOLD, KC_VOLU, KC_MUTE,          _______,
+    XXXXXXX, XXXXXXX, XXXXXXX,                             EE_CLR, XXXXXXX, QK_BOOT,                              XXXXXXX, XXXXXXX, _______
   ),
 
 };
