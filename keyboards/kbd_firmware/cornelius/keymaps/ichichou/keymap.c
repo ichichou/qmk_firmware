11aeb811947d44ac24eb8831f5bd2cb31b35f8fa
ichichou <35998815+ichichou@users.noreply.github.com>
2026-03-20 22:13:58 +0900

    Update keymaps

--- keyboards/kbd_firmware/cornelius/keymaps/ichichou/keymap.c
+++ keyboards/kbd_firmware/cornelius/keymaps/ichichou/keymap.c
@@ -29,17 +29,17 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_wrapper(
-    RHYPR_TAB,   __________________BASE_L1__________________, __________________BASE_R1__________________,     KC_BSPC,
-    LCTL_ESC,    __________________BASE_L2__________________, __________________BASE_R2__________________,     KC_QUOT,
-    KC_LSFT,     __________________BASE_L3__________________, __________________BASE_R3__________________,     KC_BSLS,
-    FN,       KC_RCTL, KC_LALT, LGUI_LNG2, NAV_ESC, LSFT_SPC, RCTL_ENT, SYM_BSPC, RGUI_LNG1, KC_RALT, KC_RCTL, FN_GRV
+    HYPR_TAB,   __________________BASE_L1__________________, __________________BASE_R1__________________,     KC_BSPC,
+    LCTL_ESC,   __________________BASE_L2__________________, __________________BASE_R2__________________,     KC_QUOT,
+    KC_LSFT,    __________________BASE_L3__________________, __________________BASE_R3__________________,     KC_BSLS,
+    FN,      KC_RCTL, KC_LALT, LGUI_LNG2, NAV_ESC, LSFT_SPC, RCTL_ENT, SYM_BSPC, RGUI_LNG1, KC_RALT, KC_RCTL, FN_GRV
   ),
 
   [_MTGAP] = LAYOUT_wrapper(
-    RHYPR_TAB,   __________________MTGAP_L1_________________, __________________MTGAP_R1_________________,     KC_BSPC,
-    LCTL_ESC,    __________________MTGAP_L2_________________, __________________MTGAP_R2_________________,     MT_QUOT,
-    KC_LSFT,     __________________MTGAP_L3_________________, __________________MTGAP_R3_________________,     MT_BSLS,
-    FN,       KC_RCTL, KC_LALT, LGUI_LNG2, NAV_ESC, LSFT_SPC, RCTL_ENT, SYM_BSPC, RGUI_LNG1, KC_RALT, KC_RCTL, FN_GRV
+    HYPR_TAB,   __________________MTGAP_L1_________________, __________________MTGAP_R1_________________,     KC_BSPC,
+    LCTL_ESC,   __________________MTGAP_L2_________________, __________________MTGAP_R2_________________,     MT_QUOT,
+    KC_LSFT,    __________________MTGAP_L3_________________, __________________MTGAP_R3_________________,     MT_BSLS,
+    FN,      KC_RCTL, KC_LALT, LGUI_LNG2, NAV_ESC, LSFT_SPC, RCTL_ENT, SYM_BSPC, RGUI_LNG1, KC_RALT, KC_RCTL, FN_GRV
   ),
 
   [_NAV] = LAYOUT_wrapper(
